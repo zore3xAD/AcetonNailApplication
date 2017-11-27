f60b219c85ea5ac7127f690c1dffcbb4f7913899
DobrogorskiyAA <zore3x@gmail.com>
2017-11-27 15:18:46 +0200

    Add fab icon

--- app/src/main/java/com/android/zore3x/acetonnailapplication/MainActivity.java
+++ app/src/main/java/com/android/zore3x/acetonnailapplication/MainActivity.java
@@ -60,6 +60,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        fillMainContent(ClientsListFragment.newInstance());
     }
 
     @Override
